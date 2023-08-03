a2d48358f84e90c82262f87fe0c0bd84cd4571e5
MikeSiebel <mzhitkov@gmail.com>
2023-08-03 13:46:34 +0300

    install composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sf/task276authpdo',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '510dc0a54c52d4fc22250cca1ad3576367ab9891',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sf/task276authpdo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '510dc0a54c52d4fc22250cca1ad3576367ab9891',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
